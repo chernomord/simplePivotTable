5691a4725c26a062df0dc58fc611d8c6ea884121
Slava <horlet@gmail.com>
2017-03-03 21:36:38 +0300

    data: row and col heads collected, result data (matched objects) in raw format gathered

--- spec/dataCrowler/DataCrowler.spec.js
+++ spec/dataCrowler/DataCrowler.spec.js
@@ -6,14 +6,19 @@ describe('Data Crawler', () => {
     it('should get rows fields', () => {
         let data = new DataCrawler();
         let rowsFields = data.getRowsArray(cars, ['year', 'department', 'brand']);
-        console.log(rowsFields);
-        expect(rowsFields.length).toBe(3);
-        expect(rowsFields[2][1]).toBe('Honda');
+        expect(rowsFields.length).toBe(27);
+        expect(rowsFields[9][0]).toBe(2015);
+    });
+    it('should return matched items', () => {
+       let data = new DataCrawler();
+       let matched = data.findMatched(cars, ['year', 'department', 'brand', 'type'], [2014, 'Moscow', 'Honda', 'sedan']);
+       expect(matched.length).toBe(1);
+       expect(matched[0].price).toBe('24000');
     });
     it('should get pivot result as a dict of arrays of the fields not included in rows and cols', () => {
         let data = new DataCrawler();
         let results = data.pivotResultsRaw(cars, ['year', 'department'], ['brand', 'type']);
-        console.log(results);
-
+        expect(results[0][0].length).toBe(1);
+        expect(results[0][0][0].price).toBe('24000');
     })
 });
--- src/dataCrawler/DataCrawler.js
+++ src/dataCrawler/DataCrawler.js
@@ -8,20 +8,88 @@ class DataCrawler {
      * @param rowNames {Array}
      * @returns {Array}
      */
-    getRowsArray(data, rowNames) {
+    getRowsArray (data, rowNames) {
         let result = [];
-        for (let row in rowNames) {
-            let rowName = rowNames[row];
-            let dummy = {};
-            result.push([]);
-            for (let item of data) {
-                dummy[item[rowName]] = item[rowName];
+        for (let item of data) {
+            let dummy = [];
+            for (let name of rowNames) {
+                dummy.push(item[name]);
             }
-            for (let item in dummy) {
-                result[row].push(item);
+            if (JSON.stringify(result).indexOf(JSON.stringify(dummy)) < 0) {
+                result.push(dummy);
             }
         }
+        result.sort((a, b) => {
+            for (let i = 0; i < a.length; ++i) {
+                if (a[i] < b[i]) return -1;
+                if (a[i] > b[i]) return 1;
+            }
+            return 0
+        });
+        return result;
+    }
+
+    /**
+     *
+     * @param data {Array}
+     * @param fields {Array}
+     * @param values {Array}
+     * @returns {Array}
+     */
+    findMatched (data, fields, values) {
+        let result = [];
+        for (let item of data) { // iterate data array
+            let objectFound = false;
+            let obj = {};
+            for (let prop in fields) { // iterate array of searched props names
+                let field = fields[prop];
+                let value = values[prop];
+                obj[field] = value;
+                if (item[field] === value) {
+                    objectFound = true
+                } else {
+                    objectFound = false; break;
+                }
+            }
+            if (objectFound) result.push(item);
+        }
         return result
+    }
+
+    /**
+     *
+     * @param data {Array}
+     * @param rowNames {Array}
+     * @param colNames {Array}
+     * @returns {Array}
+     */
+    pivotResultsRaw (data, rowNames, colNames) {
+        let result = [];
+        let allProps = rowNames.concat(colNames);
+        let rowProps = this.getRowsArray(data, rowNames);
+        let colProps = this.getRowsArray(data, colNames);
+        for (let rowIdx in rowProps) { // iterate rows values
+            result[rowIdx] = [];
+            for (let colIdx in colProps) { // iterate cols values
+                let allValues = rowProps[rowIdx].concat(colProps[colIdx]);
+                result[rowIdx][colIdx] = this.findMatched(data, allProps, allValues);
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     *
+     * @param data {Array}
+     * @param rowNames {Array}
+     * @param colNames {Array}
+     * @param calculateResultCallback {Function}
+     */
+    renderFknTable (data, rowNames, colNames, calculateResultCallback) {
+        let TABLE = document.createElement('table');
+        let THEAD = document.createElement('thead');
+        let TBODY = document.createElement('tbody');
     }
 }
 
